1567e382e301a980d3f3d645ec9473e30040aaac
mouad2023se <140627334+mouad2023se@users.noreply.github.com>
2024-06-12 23:23:51 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('contact-modal');
+    const btn = document.getElementById('contact-button');
+    const span = document.getElementsByClassName('close')[0];
+
+    btn.onclick = function() {
+        modal.style.display = 'block';
+    }
+
+    span.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
